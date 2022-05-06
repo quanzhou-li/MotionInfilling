638b339f3fba47a2dced6facc41225b7a81b6d7f
quanzhou li <qzdy369@vip.qq.com>
2022-05-06 02:41:37 -0700

    update

--- train/trainer_motion_infill.py
+++ train/trainer_motion_infill.py
@@ -137,6 +137,7 @@ class Trainer:
 
         ### Smooth loss (test)
         loss_smooth = 90. * (1. - self.cfg.kl_coef) * self.LossL2(data['I'][:, 0, :-8, 1:]-data['I'][:, 0, :-8, :-1], drec['I'][:, 0, :-8, 1:] - drec['I'][:, 0, :-8, :-1])
+        loss_smooth_drec = 90. * (1. - self.cfg.kl_coef) * tensor.norm(drec['I'][:, 0, :-8, 1:] - drec['I'][:, 0, :-8, :-1])
 
         ### KL loss traj
         q_z_traj = torch.distributions.normal.Normal(drec['mean_traj'], drec['std_traj'])
@@ -165,7 +166,8 @@ class Trainer:
             'loss_fg_contact': loss_fg_contact,
             'loss_kl_traj': loss_kl_traj,
             'loss_kl_local': loss_kl_local,
-            'loss_smooth': loss_smooth
+            'loss_smooth': loss_smooth,
+            'loss_smooth_drec': loss_smooth_drec
         }
 
         loss_total = torch.stack(list(loss_dict.values())).sum()
